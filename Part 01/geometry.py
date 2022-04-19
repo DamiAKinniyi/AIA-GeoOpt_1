dc1f28c2753c2e096b03f17a9379a0a9cafb2e2a
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-04-19 14:59:23 -0500

    Update geometry.py

--- Part 01/geometry.py
+++ Part 01/geometry.py
@@ -1,7 +1,30 @@
 import rhino3dm as rg
+import random as ra
 
-def createSphere(c,r):
-    sph = rg.Sphere(c,r)
+def createSphere(c,r,d):
+    ''' 
+    Generates a spiky sphere mesh geometry .
+    --------------------------------------
+
+    Parameters
+    -----------
+    o: sets the origin of the mesh
+    r: sets the starting radius of the mesh
+    d: sets the displacement for the mesh vertices
+    
+    Returns
+    ----------
+    A mesh geometry
+    
+    '''
+    sph = rg.Sphere(c,r,d),
     sph = sph.ToNurbsSurface()
     sph2 = sph.mesh()
+    vector1 = sph2.normals
+    disp = r.uniform(0,d)
+    pts = sph2.vertices.translation(vector1*disp)
+    faces = sph2.Faces
+    sph3 = rg.
+    
+
     return sph2
